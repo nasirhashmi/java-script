cefb1efce0a301e6a1bc8cdf8aea0e5e983f65f0
nasirhashmi <149541295+nasirhashmi@users.noreply.github.com>
2023-11-03 22:30:42 +0000

    cotainer config done

---
+++ test.js
@@ -0,0 +1,19 @@
+console.log("nasir");
+let score = '33';
+console.log(typeof (score));
+
+let valueINnumber = Number(score);
+
+console.log(typeof(valueINnumber));
+
+let userName = "adam";
+
+let userSecond = userName;
+
+ userSecond = "peter";
+
+ console.log(userName)
+ console.log(userSecond)
+ 
+
+
